f9c1ed5ac5f544cc12bf47058e5eb8710a09f6ac
Nikita Mazur <mazur.nike@gmail.com>
2018-04-27 17:47:08 +0300

    fix eslint warnings

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -8,17 +8,14 @@ export default function (state = initState, action) {
     switch (action.type) {
         case 'AUTH_USER_SUCCESS': {
             return { ...state, login: true }
-            break
         }
         
         case 'AUTH_USER_LOGOUT': {
             Cookies.expire('AuthToken');
             return { ...state, login: false }
-            break
         }
 
         default:
             return state
-        break
     }
 }
--- src/reducers/parcels.js
+++ src/reducers/parcels.js
@@ -8,21 +8,17 @@ export default function (state = initState, action) {
     switch (action.type) {
         case 'GET_PARCELS': {
             return { ...state, parcels: action.parcels, trackParcels: [] }
-            break
         }
 
         case 'GET_INCOMING_PARCELS': {
             return { ...state, incomingParcels: action.incomingParcels, trackParcels: [] }
-            break
         }
 
         case 'TRACK_PARCELS': {
             return { ...state, trackParcels: action.trackParcels }
-            break
         }
 
         default:
             return state
-        break
     }
 }
